76df2454724f102399db7cceaea078ea1f3d628b
tejasmb03 <tejasmb2003@gmail.com>
2025-06-18 18:57:25 +0530

    Updated portfolio with latest CSS and features

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -29,6 +29,15 @@ const projects = [
   },
   {
     id: 4,
+    title: "Smart Environmental Monitoring",
+    description:
+      "EnviroDash is a web-based dashboard that visualizes environmental changes using satellite and ground imagery. It offers modules for NDVI analysis, urbanization tracking, and data-driven visualizations. The app empowers users to monitor and compare ecological trends over time.",
+    image: "/envirodash.png",
+    liveDemo: "https://smart-environmental-monitoring.onrender.com",
+    github: "https://github.com/tejasmb03/Smart-Environmental-Monitoring.git",
+  },
+  {
+    id: 5,
     title: "Guess the Number",
     description:
       "A Flask-based Guess the Number web application where users have limited chances to guess a randomly generated number based on difficulty level. It features dynamic hints, session-based score tracking, and a responsive frontend interface.",
@@ -38,13 +47,14 @@ const projects = [
 
   },
    {
-    id: 5,
+    id: 6,
     title: "Weather App",
     description:
       "A React-based weather application that displays real-time weather conditions including temperature, feels-like temperature, cloud status, humidity, and min/max temperature. It fetches live data from a weather API and dynamically shows localized weather details like broken clouds and humidity for any searched city.",
     image: "/weather.png",
     liveDemo: "https://tejas2003.netlify.app/",
   },
+  
 ];
 
 const Projects = () => (
